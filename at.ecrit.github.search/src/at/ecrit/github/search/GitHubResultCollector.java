96b99769620aab35fcb0fc33eed28bfa1becb6e8
lamann <amann@medevit.at>
2014-08-04 12:26:44 +0200

    git search results collector autorestarts now

--- at.ecrit.github.search/src/at/ecrit/github/search/GitHubResultCollector.java
+++ at.ecrit.github.search/src/at/ecrit/github/search/GitHubResultCollector.java
@@ -10,7 +10,6 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,63 +19,79 @@ import at.ecrit.github.evaluation.GitHubConnector;
 
 public class GitHubResultCollector {
 	private static final String GIT_BASE = "https://github.com";
-	private static File linkSaver;
-	private static List<String> appModelLinks;
-	private static List<String> processed;
+	private File linkSaver;
+	private List<String> appModelLinks;
+	private List<String> processed;
 	
-	public static void findApplicationModelLinks(List<String> resultPages,
-		GitHubConnector gitConnector, String linkFilePath) throws InterruptedException{
-		initLinkSaverFile(linkFilePath);
+	public GitHubResultCollector(File linkSaver){
+		this.linkSaver = linkSaver;
+	}
+	
+	/**
+	 * finds all the application model links and manages their storage in a text-file
+	 * 
+	 * @param resultPages
+	 *            list of all pages to evaluate
+	 * @param gitConnector
+	 *            holds user credentials
+	 * @return result page where to start next or 0 if finished
+	 * @throws InterruptedException
+	 */
+	public int findApplicationModelLinks(List<String> resultPages, GitHubConnector gitConnector)
+		throws InterruptedException{
 		appModelLinks = loadExistingLinks();
 		processed = new ArrayList<String>();
 		
 		for (String page : resultPages) {
-			loadLinks(page, gitConnector);
+			try {
+				loadLinks(page, gitConnector);
+			} catch (IOException | URISyntaxException e) {
+				int startIdx = page.indexOf("p=") + 2;
+				int endIdx = page.indexOf("&", startIdx);
+				int nextStart = Integer.parseInt(page.substring(startIdx, endIdx));
+				
+				e.printStackTrace();
+				return nextStart;
+			}
 		}
-		System.out.println("DONE!");
+		return 0;
 	}
 	
-	private static void loadLinks(String page, GitHubConnector gitConnector){
+	private void loadLinks(String page, GitHubConnector gitConnector) throws IOException,
+		URISyntaxException{
 		if (processed.contains(page))
 			return;
 		
 		int counter = 0;
-		try {
-			Document doc = Jsoup.parse(gitConnector.getGETPageContent(page));
-			// Document doc = Jsoup.connect(page).get();
-			
-			Elements links = doc.select("a[href]");
-			processed.add(page);
-			
-			for (Element link : links) {
-				String linkType = link.text();
-				if (linkType.endsWith("e4xmi")) {
-					String repoUrl = GIT_BASE + link.parent().select("a[href]").attr("href");
-					String appModelUrl = GIT_BASE + link.attr("href");
-					
-					if (appModelLinks.contains(appModelUrl)) {
-						System.out.println("Already added: " + appModelUrl);
-						continue;
-					}
-					
-					appModelLinks.add(appModelUrl);
-					writeToFile(repoUrl, appModelUrl);
-					counter++;
-// System.out.println("+ RepoURL: " + repoUrl + " AppModelURL: " + appModelUrl);
+		Document doc = Jsoup.parse(gitConnector.getGETPageContent(page));
+		// Document doc = Jsoup.connect(page).get();
+		
+		Elements links = doc.select("a[href]");
+		processed.add(page);
+		
+		for (Element link : links) {
+			String linkType = link.text();
+			if (linkType.endsWith("e4xmi")) {
+				String repoUrl = GIT_BASE + link.parent().select("a[href]").attr("href");
+				String appModelUrl = GIT_BASE + link.attr("href");
+				
+				if (appModelLinks.contains(appModelUrl)) {
+					System.out.println("Already added: " + appModelUrl);
+					continue;
 				}
+				
+				appModelLinks.add(appModelUrl);
+				writeToFile(repoUrl, appModelUrl);
+				counter++;
+				// System.out.println("+ RepoURL: " + repoUrl + " AppModelURL: " + appModelUrl);
 			}
-		} catch (IOException | URISyntaxException hse) {
-			int startIdx = page.indexOf("p=") + 2;
-			int endIdx = page.indexOf("&");
-			System.out.println("Please restart with page: " + page.substring(startIdx, endIdx));
-			System.out.println("Status: " + ((HttpStatusException) hse).getStatusCode() + hse);
-			
 		}
 		if (counter < 10)
 			System.out.println("Links added: " + counter + "\nPage: " + page);
+		
 	}
 	
-	private static void writeToFile(String repoUrl, String appModelUrl){
+	private void writeToFile(String repoUrl, String appModelUrl){
 		try {
 			FileWriter fw = new FileWriter(linkSaver.getAbsoluteFile(), true);
 			BufferedWriter bw = new BufferedWriter(fw);
@@ -88,18 +103,8 @@ public class GitHubResultCollector {
 		}
 	}
 	
-	private static void initLinkSaverFile(String linkFilePath){
-		try {
-			linkSaver = new File(linkFilePath);
-			if (linkSaver.exists())
-				linkSaver.createNewFile();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static List<String> loadExistingLinks(){
+	@SuppressWarnings("resource")
+	private List<String> loadExistingLinks(){
 		List<String> linkList = new ArrayList<>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(linkSaver));
